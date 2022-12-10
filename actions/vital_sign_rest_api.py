6ca083276b07352182dfdbb0f57b78edb7c06450
akramaliomar <65216643+akramaliomar@users.noreply.github.com>
2022-12-10 22:13:49 +0200

    Update vital_sign_rest_api.py

--- actions/vital_sign_rest_api.py
+++ actions/vital_sign_rest_api.py
@@ -1,9 +1,24 @@
 import requests, json
 
 
-def fetch_vital_signs():
-    r = requests.post('http://154.53.40.198:8080/api/fetch_vs', json={'deviceNo': "DVS0003"})
+def fetch_vital_signs(device_no):
+    r = requests.post('http://154.53.40.198:8080/api/fetch_vs', json={'deviceNo': device_no})
     return r.json()
+
+
+def authenticate_device(device_no, device_code):
+    r = requests.post('http://154.53.40.198:8080/api/authenticate_device_user', json={'deviceNo': device_no, "device_key": device_code})
+    return r.json()
+
+
+def optimize_slots(myslot):
+    if type(myslot) == list:
+        if "DVS" in myslot:
+            myslot.remove("DVS")
+        return ''.join(myslot)
+    else:
+        return myslot
+
 
 
 # def prediction(hr, spo2, resp, tempr):
@@ -37,8 +52,8 @@ def fetch_vital_signs():
 #     return json_data
 
 
-def check_anomalies():
-    r = requests.post('http://154.53.40.198:8080/api/predict_anomaly', json={'deviceNo': "DVS0003"})
+def check_anomalies(device_no):
+    r = requests.post('http://154.53.40.198:8080/api/predict_anomaly', json={'deviceNo': device_no})
     # return json.dumps(r.json())
     return r.json()
     # print(json.dumps(json_data.json()))
@@ -49,8 +64,8 @@ def check_anomalies():
     # # format_add = json_data['main']
 
 
-def check_anomaly_recommendations():
-    r = requests.post('http://154.53.40.198:8080/api/get_recommendtions_vs', json={'deviceNo': "DVS0003"})
+def check_anomaly_recommendations(device_no, age, context, health):
+    r = requests.post('http://154.53.40.198:8080/api/get_recommendtions_vs', json={'deviceNo': device_no, 'age': age, 'context': context, 'health': health})
     # json_data = requests.get(url).json()
     # format_add = json_data['main']
     # print(format_add)
